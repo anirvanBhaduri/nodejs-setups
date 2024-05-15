f7e8528e8c9ac8af3e2e1f32483c7a1472ec0dcb
Anirvan Bhaduri <anirvan.bhaduri@gmail.com>
2024-05-15 21:19:05 +0200

    Added some pseudo css styling.

--- frontend/app/App.tsx
+++ frontend/app/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
   return (
     <>
-      <h1 className="test-content">"Some content in the middle right here"</h1>
+      <h1 className="test-content">Some content in the middle right here</h1>
     </>
   );
 };
